293ba27363401a1b16e202b8b0069bc8f43d4bae
Swiftter7 <42277231+Swiftter7@users.noreply.github.com>
2018-08-16 16:18:03 +0800

    All traps except falling and fan switch
    yay

--- SP1Framework/SP1Framework/game.cpp
+++ SP1Framework/SP1Framework/game.cpp
@@ -20,6 +20,8 @@ char mapStorage[100][100];
 int NewX = 0, NewY = 28;
 int NumberDIE = 0;
 std::string NumberOfLives;
+int LevelSelected = 0;
+int ChangesArrayOne[50] = { 0, };
 
 // Game specific variables here
 SGameChar   g_sChar;
@@ -47,10 +49,6 @@ void init( void )
 
     // sets the initial state for the game
     g_eGameState = S_GAMEMENU;		//18   13		8
-
-    g_sChar.m_cLocation.X = 0;
-    g_sChar.m_cLocation.Y = 28;
-    g_sChar.m_bActive = true;
 
 	initGameAsset();
 
@@ -173,7 +171,13 @@ void gameMenu()    // waits for user choice
 	}
 	if (g_abKeyPressed[K_ENTER]) { // Press enter to start game
 		switch (Choice) {
-		case 1: g_eGameState = S_GAME;
+		case 1: LevelSelected = 1; // set LevelSelected values (for hard-coding level assets)
+
+			g_sChar.m_cLocation.X = 0; // initialise player for Level 1 (Easy Mode)
+			g_sChar.m_cLocation.Y = 28;
+			g_sChar.m_bActive = true;
+
+			g_eGameState = S_GAME;
 			break;
 		case 3: g_bQuitGame = true;
 			break;
@@ -206,8 +210,11 @@ if (g_abKeyPressed[K_UP] && g_sChar.m_cLocation.Y > 2) // FOR "UP"
 	//Beep(1440, 30);
 	if (mapStorage[(int)g_sChar.m_cLocation.Y - 2][(int)g_sChar.m_cLocation.X] != '#')
 	{
-		g_sChar.m_cLocation.Y--;
-		bSomethingHappened = true;
+		if (mapStorage[(int)g_sChar.m_cLocation.Y - 2][(int)g_sChar.m_cLocation.X] != 'D')
+		{
+			g_sChar.m_cLocation.Y--;
+			bSomethingHappened = true;
+		}	
 	}
 }
 if (g_abKeyPressed[K_LEFT] && g_sChar.m_cLocation.X > 0 && g_sChar.m_cLocation.X < 79) // FOR "LEFT"
@@ -215,8 +222,11 @@ if (g_abKeyPressed[K_LEFT] && g_sChar.m_cLocation.X > 0 && g_sChar.m_cLocation.X
 	//Beep(1440, 30);
 	if (mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X - 1] != '#')
 	{
-		g_sChar.m_cLocation.X--;
-		bSomethingHappened = true;
+		if (mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X - 1] != 'D')
+		{
+			g_sChar.m_cLocation.X--;
+			bSomethingHappened = true;
+		}
 	}
 
 }
@@ -225,8 +235,11 @@ if (g_abKeyPressed[K_DOWN] && g_sChar.m_cLocation.Y < g_Console.getConsoleSize()
 	//Beep(1440, 30);
 	if (mapStorage[(int)g_sChar.m_cLocation.Y][(int)g_sChar.m_cLocation.X] != '#')
 	{
-		g_sChar.m_cLocation.Y++;
-		bSomethingHappened = true;
+		if (mapStorage[(int)g_sChar.m_cLocation.Y][(int)g_sChar.m_cLocation.X] != 'D') 
+		{
+			g_sChar.m_cLocation.Y++;
+			bSomethingHappened = true;
+		}
 	}
 
 }
@@ -235,8 +248,11 @@ if (g_abKeyPressed[K_RIGHT] && g_sChar.m_cLocation.X < g_Console.getConsoleSize(
 	//Beep(1440, 30);
 	if (mapStorage[(int)g_sChar.m_cLocation.Y-1][(int)g_sChar.m_cLocation.X+1] != '#')
 	{
-		g_sChar.m_cLocation.X++;
-		bSomethingHappened = true;
+		if (mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 1] != 'D')
+		{
+			g_sChar.m_cLocation.X++;
+			bSomethingHappened = true;
+		}
 	}
 
 }
@@ -246,7 +262,17 @@ if (g_abKeyPressed[K_SPACE])
 	bSomethingHappened = true;
 }
 
-if (mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 9] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 8] == 'N' ||
+if (/*mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 29] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 28] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 27] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 26] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 25] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 24] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 23] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 22] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 21] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 20] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 19] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 18] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 17] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 16] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 15] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 14] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 13] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 12] == 'N' ||
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 11] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 10] == 'N' ||*/
+	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 9] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 8] == 'N' ||
 	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 7] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 6] == 'N' ||
 	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 5] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 4] == 'N' ||
 	mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 3] == 'N' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 2] == 'N' ||
@@ -256,7 +282,7 @@ if (mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 9] =
 	{
 		FanBlowLeftDelay++;
 	}
-	else if (FanBlowLeftDelay == AllowedMaxFanDelay && mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X - 1] != '#')
+	else if (FanBlowLeftDelay == AllowedMaxFanDelay && mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X - 1] != '#' && mapStorage[(int)g_sChar.m_cLocation.Y - 2][(int)g_sChar.m_cLocation.X] != 'D')
 	{
 		if (g_sChar.m_cLocation.X > 0 && g_sChar.m_cLocation.X < 79)
 		{
@@ -279,7 +305,7 @@ if (mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X - 9] =
 	}
 	else if (FanBlowRightDelay == AllowedMaxFanDelay)
 	{
-		if (g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 41 && mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 1] != '#')
+		if (g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 41 && mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X + 1] != '#' && mapStorage[(int)g_sChar.m_cLocation.Y - 2][(int)g_sChar.m_cLocation.X] != 'D')
 		{
 			g_sChar.m_cLocation.X++;
 		}
@@ -300,7 +326,7 @@ if (mapStorage[(int)g_sChar.m_cLocation.Y + 9][(int)g_sChar.m_cLocation.X] == 'Z
 	}
 	else if (FanBlowUpDelay == AllowedMaxFanDelay)
 	{
-		if (g_sChar.m_cLocation.Y > 2 && mapStorage[(int)g_sChar.m_cLocation.Y - 2][(int)g_sChar.m_cLocation.X] != '#')
+		if (g_sChar.m_cLocation.Y > 2 && mapStorage[(int)g_sChar.m_cLocation.Y - 2][(int)g_sChar.m_cLocation.X] != '#' && mapStorage[(int)g_sChar.m_cLocation.Y - 2][(int)g_sChar.m_cLocation.X] != 'D')
 		{
 			g_sChar.m_cLocation.Y--;
 		}
@@ -321,7 +347,7 @@ if (mapStorage[(int)g_sChar.m_cLocation.Y - 9][(int)g_sChar.m_cLocation.X] == 'X
 	}
 	else if (FanBlowDownDelay == AllowedMaxFanDelay)
 	{
-		if (g_sChar.m_cLocation.Y < g_Console.getConsoleSize().Y - 7 && mapStorage[(int)g_sChar.m_cLocation.Y][(int)g_sChar.m_cLocation.X] != '#')
+		if (g_sChar.m_cLocation.Y < g_Console.getConsoleSize().Y - 7 && mapStorage[(int)g_sChar.m_cLocation.Y][(int)g_sChar.m_cLocation.X] != '#' && mapStorage[(int)g_sChar.m_cLocation.Y - 2][(int)g_sChar.m_cLocation.X] != 'D')
 		{
 			g_sChar.m_cLocation.Y++;
 		}
@@ -331,13 +357,14 @@ if (mapStorage[(int)g_sChar.m_cLocation.Y - 9][(int)g_sChar.m_cLocation.X] == 'X
 }
 
 
+
 if (bSomethingHappened)
 {
 	// set the bounce time to some time in the future to prevent accidental triggers
 	g_dBounceTime = g_dElapsedTime + 0.125; // 125ms should be enough
 
 
-	if (mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X] == 'S') //TRAP "SPIKE" 'y-1'
+	if (mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X] == 'S' || mapStorage[(int)g_sChar.m_cLocation.Y - 1][(int)g_sChar.m_cLocation.X] == '!') //TRAP "SPIKE" 'y-1'
 	{
 		RespawnAt();
 		NumberDIE++;
@@ -353,6 +380,63 @@ if (bSomethingHappened)
 	{
 		NewX = g_sChar.m_cLocation.X;
 		NewY = g_sChar.m_cLocation.Y;
+	}
+
+	if (LevelSelected == 1) // FOR FIRST LEVEL
+	{
+		if ((int)g_sChar.m_cLocation.Y - 1 == 18 && (int)g_sChar.m_cLocation.X == 57) // for first falling trap
+		{
+			ChangesArrayOne[0] = 1;
+		}
+		if ( ( (int)g_sChar.m_cLocation.Y - 1 == 27 && (int)g_sChar.m_cLocation.X == 28 ) || ( (int)g_sChar.m_cLocation.Y - 1 == 25 && (int)g_sChar.m_cLocation.X == 28) )
+		{
+			ChangesArrayOne[1] = 1; // for second 2 pressure plates
+		}
+		if ( (int)g_sChar.m_cLocation.Y - 1 == 23 && (int)g_sChar.m_cLocation.X == 70) // for pressure plate after "2 fake pressure plate"
+		{
+			ChangesArrayOne[2] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 19 && (int)g_sChar.m_cLocation.X == 64) // for top left at room with 2nd checkpoint
+		{
+			ChangesArrayOne[3] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 23 && (int)g_sChar.m_cLocation.X == 59) // for bottome left at room with 2nd checkpoint
+		{
+			ChangesArrayOne[4] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 18 && (int)g_sChar.m_cLocation.X == 78) // for top right at room with 2nd checkpoint
+		{
+			ChangesArrayOne[5] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 13 && (int)g_sChar.m_cLocation.X == 78) // for rightmost generator
+		{
+			ChangesArrayOne[6] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 16 && (int)g_sChar.m_cLocation.X == 71) // for pressure plate between spikes
+		{
+			ChangesArrayOne[7] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 14 && (int)g_sChar.m_cLocation.X == 1) // for leftmost pressure plate
+		{
+			ChangesArrayOne[8] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 15 && (int)g_sChar.m_cLocation.X == 1) // for leftmost generator
+		{
+			ChangesArrayOne[9] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 10 && (int)g_sChar.m_cLocation.X == 16 || (int)g_sChar.m_cLocation.Y - 1 == 10 && (int)g_sChar.m_cLocation.X == 17) // for falling trap row room pressure plate
+		{
+			ChangesArrayOne[10] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 10 && (int)g_sChar.m_cLocation.X == 49) // for spike room generator
+		{
+			ChangesArrayOne[11] = 1;
+		}
+		if ((int)g_sChar.m_cLocation.Y - 1 == 10 && (int)g_sChar.m_cLocation.X == 52) // for spike room leftmost pressure plate
+		{
+			ChangesArrayOne[12] = 1;
+		}		
+
 	}
 }
 }
@@ -396,7 +480,6 @@ void renderGameMenu()  // renders the game menu	//TODO: change this to game menu
 	case 1: g_Console.writeToBuffer(c, "->", 0x03);
 		break;
 	case 2: c.Y += 1;
-
 		g_Console.writeToBuffer(c, "->", 0x03);
 		break;
 	case 3: c.Y += 2;
@@ -424,22 +507,97 @@ void renderMap()
 
 	COORD c;
 	string line;
-	ifstream myfile("maze.txt");
+	
 	int i = 0;
 	int pos = 0;
-	if (myfile.is_open())
-	{
-		while (getline(myfile, line))
-		{
-			for (int j = 0; j < 80; j++)
-			{
-				mapStorage[i][j] = line[j]; // WHY IS IT Y,X
-			}
-			i++;
-		}
-		myfile.close();
-	}
 
+	if (LevelSelected == 1) // FOR LEVEL ONE
+	{
+		ifstream myfile("maze.txt");
+		if (myfile.is_open())
+		{
+			while (getline(myfile, line))
+			{
+				for (int j = 0; j < 80; j++)
+				{
+					mapStorage[i][j] = line[j]; // WHY IS IT Y,X
+				}
+				i++;
+			}
+			myfile.close();
+		}
+
+		for (int i = 0; i < 50; i++) // FOR LEVEL ONE
+		{
+			if (ChangesArrayOne[0] == 1)
+			{
+				// add first falling traps
+			}
+			if (ChangesArrayOne[1] == 1)
+			{
+				mapStorage[26][68] = ' '; // opens 1st door
+			}
+			if (ChangesArrayOne[2] == 1)
+			{
+				mapStorage[22][78] = ' '; // removes bottom right spike at 2nd checkpoint room
+			}
+			if (ChangesArrayOne[3] == 1)
+			{
+				mapStorage[23][64] = ' ';// removes bottom left spike at 2nd checkpoint room
+			}
+			if (ChangesArrayOne[4] == 1)
+			{
+				mapStorage[20][71] = ' '; // removes top right spike at 2nd checkpoint room
+			}
+			if (ChangesArrayOne[5] == 1)
+			{
+				mapStorage[17][59] = ' '; // opens 2nd door
+			}
+			if (ChangesArrayOne[6] == 1)
+			{
+				mapStorage[12][58] = ' ', mapStorage[12][59] = ' ', mapStorage[12][60] = ' ', mapStorage[12][61] = ' ', mapStorage[12][62] = ' ', mapStorage[12][63] = ' ', mapStorage[12][64] = ' ', mapStorage[12][65] = ' ', mapStorage[12][66] = ' ', mapStorage[12][67] = ' ', mapStorage[12][68] = ' ', mapStorage[12][69] = ' ', mapStorage[12][70] = ' ', mapStorage[12][71] = ' ', mapStorage[12][72] = ' ';
+				mapStorage[13][58] = ' ';
+				mapStorage[14][58] = ' ', mapStorage[14][60] = ' ', mapStorage[14][61] = ' ', mapStorage[14][62] = ' ', mapStorage[14][63] = ' ', mapStorage[14][64] = ' ', mapStorage[14][65] = ' ', mapStorage[14][66] = ' ', mapStorage[14][67] = ' ', mapStorage[14][68] = ' ', mapStorage[14][69] = ' ', mapStorage[14][70] = ' ', mapStorage[14][71] = ' ', mapStorage[14][72] = ' ';
+				mapStorage[15][58] = ' ', mapStorage[15][60] = ' ';
+				mapStorage[16][58] = ' ', mapStorage[16][60] = ' '; // turns off rightmost electric floors (Coding order follows map display order)
+			}
+			if (ChangesArrayOne[7] == 1)
+			{
+				mapStorage[14][43] = ' ', mapStorage[15][43] = ' '; // opens 3rd door (double door)
+			}
+			if (ChangesArrayOne[8] == 1)
+			{
+				mapStorage[11][16] = ' ', mapStorage[11][17] = ' '; // opens 4th door (double door)
+			}
+			if (ChangesArrayOne[9] == 1)
+			{
+				mapStorage[12][2] = ' '; // removes electric floor behind moving traps
+				mapStorage[13][2] = ' ', mapStorage[13][5] = ' ';
+										mapStorage[14][5] = ' ';
+										mapStorage[15][5] = ' ';
+				mapStorage[16][2] = ' ', mapStorage[16][5] = ' ';
+				mapStorage[17][2] = ' ';
+			}
+			if (ChangesArrayOne[10] == 1)
+			{
+				mapStorage[5][39] = ' ', mapStorage[6][39] = ' '; // opens 5th door between electric floors (room with row of falling traps) (double door)
+				// add 2nd falling trap
+			}
+			if (ChangesArrayOne[11] == 1)
+			{
+				mapStorage[9][54] = ' ', mapStorage[9][55] = ' ', mapStorage[9][56] = ' ', mapStorage[9][57] = ' ', mapStorage[9][58] = ' ', mapStorage[9][59] = ' ', mapStorage[9][60] = ' ', mapStorage[9][61] = ' ', mapStorage[9][62] = ' ', mapStorage[9][63] = ' ', mapStorage[9][64] = ' ', mapStorage[9][65] = ' ', mapStorage[9][66] = ' ', mapStorage[9][67] = ' ';
+				mapStorage[4][73] = ' ', mapStorage[4][74] = ' ', mapStorage[4][75] = ' '; // removes electric floor near row of pressure plates and infront of final door in 2nd last room
+			}
+			if (ChangesArrayOne[12] == 1)
+			{
+				mapStorage[3][74] = ' ', mapStorage[3][75] = ' '; // opens last door (double door)
+			}
+
+		}
+		
+
+	}
+	
 
 	for (int k = 0; k < 30; k++) {
 		int pos2 = 0;
@@ -465,7 +623,11 @@ void renderMap()
 			{
 				g_Console.writeToBuffer(c, mapStorage[k][j], 0x21);
 			}
-			else if (mapStorage[k][j] == 'Z' || mapStorage[k][j] == 'X' || mapStorage[k][j] == 'N' || mapStorage[k][j] == 'M')
+			else if (mapStorage[k][j] == '!') // fake pressure plate // act as spikes
+			{
+				g_Console.writeToBuffer(c, 'P', 0x21);
+			}
+			else if (mapStorage[k][j] == 'Z' || mapStorage[k][j] == 'X' || mapStorage[k][j] == 'N' || mapStorage[k][j] == 'M') // fans
 			{
 				g_Console.writeToBuffer(c, 'F', 0xE0);
 			}
