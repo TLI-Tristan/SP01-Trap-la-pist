b045076f8f8c0871200ef2c830070131bcccc097
Swiftter7 <42277231+Swiftter7@users.noreply.github.com>
2018-08-24 14:33:48 +0800

    Redo Fans Functions
    - Fan Function is now "soft-coded"
    - Now Fans cannot blow through walls
    - Fans are more powerful

--- SP1Framework/SP1Framework/GameAsset.h
+++ SP1Framework/SP1Framework/GameAsset.h
@@ -24,12 +24,6 @@ void renderFallingTrap(Console & g_Console, SGameTrap g_fTrap[34]);
 void initFallingTrap(struct SGameTrap g_fTrap[34]);
 void resetTrap(bool &bGotTrapPos, SGameTrap g_fTrap[34]);
 
-void FanFunctionLeft(struct SGameChar &playerInfo, char mapStorage[100][100]);							 // Left fan function (movement & some checking)
-void FanFunctionRight(struct SGameChar &playerInfo, char mapStorage[100][100], Console &g_console);		 // Right fan function (movement & some checking)
-void FanFunctionUp(struct SGameChar &playerInfo, char mapStorage[100][100]);							 // Up fan function (movement & some checking)
-void FanFunctionDown(struct SGameChar &playerInfo, char mapStorage[100][100], Console &g_console);		 // Down function (movement & some checking)
-void FanFunctionMain(struct SGameChar &playerInfo, char mapStorage[100][100], Console &g_console);		 // Main fan function (checking)
-
 void ArrayLevelOneDetect(struct SGameChar &playerInfo, int ChangesArrayOne[50]); // traps array detection (WIP)
 void ArrayLevelOneActivate(struct SGameChar &playerInfo, int ChangesArrayOne[50], char mapStorage[100][100], struct SGameTrap g_fTrap[34], enum EGAMESTATES &g_eGameState); // traps array activate (WIP)
 
@@ -37,5 +31,13 @@ void StalkerFunctionMain(struct SGameChar &playerInfo, char mapStorage[100][100]
 void StalkerFunctionMovement(double &sTrapTime, struct SGameChar &playerInfo, char mapStorage[100][100], SGameTrap g_sStalkerTrap[7]);
 void getStalkerTrapPos(char map[100][100], struct SGameTrap g_sStalkerTrap[7]);
 void renderStalkerTrap(Console &g_Console, struct SGameTrap g_sStalkerTrap[7]);
+void resetStalkerTrap(bool &bGotTrapPos2);
+
+void getFanTrapPos(char map[100][100], struct SGameTrap g_leftFanTrap[5], struct SGameTrap g_rightFanTrap[5], struct SGameTrap g_upFanTrap[5], struct SGameTrap g_downFanTrap[5]);
+void FanFunctionMain(struct SGameChar &playerInfo, char mapStorage[100][100], Console &g_console, struct SGameTrap g_leftFanTrap[5], struct SGameTrap g_rightFanTrap[5], struct SGameTrap g_upFanTrap[5], struct SGameTrap g_downFanTrap[5]);
+void leftFanMovement(double &leftFanTrapTime, struct SGameChar &playerInfo, char mapStorage[100][100], SGameTrap g_leftFanTrap[5]);
+void rightFanMovement(double &rightFanTrapTime, struct SGameChar &playerInfo, char mapStorage[100][100], SGameTrap g_rightFanTrap[5]);
+void upFanMovement(double &upFanTrapTime, struct SGameChar &playerInfo, char mapStorage[100][100], SGameTrap g_upFanTrap[5]);
+void downFanMovement(double &downFanTrapTime, struct SGameChar &playerInfo, char mapStorage[100][100], SGameTrap g_downFanTrap[5]);
 
 #endif
